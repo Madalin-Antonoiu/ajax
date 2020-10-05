ff3a69681ebeffdf617730668877bc77118a402e
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-05 09:39:43 +0300

    With form!

--- ajax.js
+++ ajax.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
 
-    fetch("https://api.exchangeratesapi.io/latest?base=USD")
-    .then(response =>  response.json()) // return in one line
-    .then(data => {
-        const rate = data.rates.EUR;
-        document.querySelector("body").innerHTML = `1 USD is equal to ${rate.toFixed(3)} EUR.`
-    })
+    document.querySelector("form").onsubmit = function(){
+
+        fetch("https://api.exchangeratesapi.io/latest?base=USD")
+        .then(response =>  response.json()) // return in one line
+        .then(data => {
+            const currency = document.querySelector("#currency").value;
+            const rate = data.rates[currency];
+            const result = document.querySelector("#result");
+            rate ? result.innerHTML = `1 USD is equal to ${rate.toFixed(3)} ${currency}` : result.innerHTML = `Invalid currency.`
+            
+        })
+
+        return false; // we dont submit to other page, we want to run locally
+    }
+
+
 
 });
