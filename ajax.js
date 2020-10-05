8751be16918907daca6b8ebcb4f4081163676231
@madalin-antonoiu <antonoiumadalin@gmail.com>
2020-10-05 09:07:17 +0300

    With writing to DOM

--- ajax.js
+++ ajax.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let t0 = performance.now()
 
     fetch("https://api.exchangeratesapi.io/latest?base=USD")
     .then(response =>  response.json()) // return in one line
-    .then(data => console.log(data))
+    .then(data => {
+        const rate = data.rates.EUR;
+        document.querySelector("body").innerHTML = `1 USD is equal to ${rate.toFixed(3)} EUR.`
+    })
 
-    let t1 = performance.now()
-    console.log("Call to fetch API data took " + (t1 - t0) + " milliseconds.")
 });
